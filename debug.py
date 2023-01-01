3f472b4702a3b116f54e375911422863142f8b86
Srikanth Babu <srikanthbabu@gmail.com>
2023-01-01 16:40:52 +0530

    Added commands

--- debug.py
+++ debug.py
@@ -2,3 +2,22 @@
 # pdb commands
 # h - help
 # b <line number> - breakpoint
+# n - move to next line
+# s - step into a function
+# until <line number>  - debugger run until that line nunber
+# c - continue
+# p <variable> - print variable
+# l - list the code around the current statement
+# bt - call statck
+# up - moves focus to one level up on the call stack
+# q - quit
+
+
+#debug using gbd
+# ps a | grep python
+# gbd python <id>
+# py-list
+# py-bt
+# py-up
+# py-down
+# py-print
