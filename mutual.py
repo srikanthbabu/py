6621755e046083ec61b0c91c47830e9e6e99f35f
SrikanthBabu <srikanthbabu@gmail.com>
2025-08-06 14:06:51 +0530

    fixed pylint,flake8,ruff warnings/errors

--- mutual.py
+++ mutual.py
@@ -1,25 +1,37 @@
-import requests
+"""
+Helper function to get mutual fund value
+Author: Srikanth Babu
+Date: 2025-08-06
+"""
 import re
+import requests
+
 
 def get_nippon_smallcap_nav():
+    """
+    To get nav values from amfiindia
+    """
     url = "https://www.amfiindia.com/spages/NAVAll.txt"
-    response = requests.get(url)
+    response = requests.get(url, timeout=10)
     data = response.text
-    
-    # print(data)
+
     # Search for the specific scheme name
-    pattern = re.compile(r"Nippon India Nifty Smallcap 250 Index Fund - Direct Plan \(G\).+?;(.+)", re.IGNORECASE)
-    pattern = re.compile(r"Nippon India Nifty Smallcap 250 .+?;(.+)", re.IGNORECASE)
+    pattern = re.compile(
+        r"Nippon India Nifty Smallcap 250 Index Fund - Direct Plan "
+        r"\(G\).+?;(.+)",
+        re.IGNORECASE
+        )
+    pattern = re.compile(r"Nippon India Nifty Smallcap 250 .+?;(.+)",
+                         re.IGNORECASE)
     match = pattern.search(data)
-    # print(match)
+
     if match:
         nav = match.group(1).strip()
         return nav.split(';')[0]
-    else:
-        print("NAV not found. Scheme name might have changed or is unavailable.")
 
-get_nippon_smallcap_nav()
+    print("NAV not found. Scheme name might have changed or is unavailable.")
+    return None
+
 
 if __name__ == "__main__":
-   
     print(get_nippon_smallcap_nav())
